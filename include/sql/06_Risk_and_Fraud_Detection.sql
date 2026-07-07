eec60f6595e1e4594a23e591d3bd3fcde8d8e62f
Subagio Setio Nugroho <74775318+subagio99@users.noreply.github.com>
2026-07-07 15:31:35 +0700

    Update 06_Risk_and_Fraud_Detection.sql

--- include/sql/06_Risk_and_Fraud_Detection.sql
+++ include/sql/06_Risk_and_Fraud_Detection.sql
@@ -11,19 +11,3 @@ WHERE UPPER(f.status) = 'FAILED'
 GROUP BY f.transaction_date, c.customer_id, c.full_name
 HAVING COUNT(f.transaction_id) >= 5 
 ORDER BY total_failed_attempts DESC;
-
-
--- 2. Deteksi Lonjakan Nilai Transaksi Sangat Besar (Anomali Nilai)
-SELECT 
-    f.transaction_id,
-    f.transaction_code,
-    c.full_name,
-    f.transaction_type,
-    f.amount,
-    ch.channel_name
-FROM fact_transactions f
-JOIN dim_accounts a ON f.account_id = a.account_id
-JOIN dim_customers c ON a.customer_id = c.customer_id
-JOIN dim_channels ch ON f.channel_id = ch.channel_id
-WHERE f.amount > 100000000 
-ORDER BY f.amount DESC;
